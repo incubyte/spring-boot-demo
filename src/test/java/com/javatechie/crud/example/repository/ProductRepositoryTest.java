7f6cc38a6fdacccca446b3dc6816d58ca39bb275
Visrut <visrut@incubyte.co>
2023-11-27 18:23:49 +0530

    Add: assertions statement for the object

--- src/test/java/com/javatechie/crud/example/repository/ProductRepositoryTest.java
+++ src/test/java/com/javatechie/crud/example/repository/ProductRepositoryTest.java
@@ -2,6 +2,7 @@ package com.javatechie.crud.example.repository;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith; // Not Given By AI
+import static org.junit.jupiter.api.Assertions.*;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -14,7 +15,6 @@ import javax.persistence.TypedQuery;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest
 class ProductRepositoryTest {
 
     @MockBean
@@ -27,10 +27,15 @@ class ProductRepositoryTest {
     void testFindByName() {
         String name = "test";
         String jpql = "SELECT p FROM Product p WHERE p.name = :name";
+        Product mockProduct = new Product();
+        mockProduct.setId(1);
+        mockProduct.setName("product1");
+        mockProduct.setPrice(22);
+        mockProduct.setQuantity(20);
 
         when(entityManager.createQuery(jpql, Product.class)).thenReturn(query);
         when(query.setParameter("name", name)).thenReturn(query);
-        when(query.getSingleResult()).thenReturn(new Product());
+        when(query.getSingleResult()).thenReturn(mockProduct);
 
         ProductRepository productRepository = new ProductRepository();
         productRepository.entityManager = entityManager;
@@ -40,5 +45,10 @@ class ProductRepositoryTest {
         verify(entityManager, times(1)).createQuery(jpql, Product.class);
         verify(query, times(1)).setParameter("name", name);
         verify(query, times(1)).getSingleResult();
+
+        assertEquals(result.getName(), "product1");
+        assertEquals(result.getId(), 1);
+        assertEquals(result.getPrice(), 22);
+        assertEquals(result.getQuantity(), 20);
     }
 }
