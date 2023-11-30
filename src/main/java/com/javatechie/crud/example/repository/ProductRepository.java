d1ad8589c32bd18bfed4f81ebbec3fae52fa7bbc
Visrut <visrut@incubyte.co>
2023-11-30 19:19:23 +0530

    fix applied

--- src/main/java/com/javatechie/crud/example/repository/ProductRepository.java
+++ src/main/java/com/javatechie/crud/example/repository/ProductRepository.java
@@ -1,3 +1,4 @@
+```java
 package com.javatechie.crud.example.repository;
 
 import com.javatechie.crud.example.entity.Product;
@@ -15,9 +16,9 @@ public class ProductRepository {
   public EntityManager entityManager;
 
   public Product findByName(String name) {
-      // WARNING: The following line is vulnerable to SQL injection!
-      String sql = "SELECT p FROM Product p WHERE p.name = '" + name + "'";
-      return entityManager.createQuery(sql, Product.class)
+      String jpql = "SELECT p FROM Product p WHERE p.name = :name";
+      return entityManager.createQuery(jpql, Product.class)
+          .setParameter("name", name)
           .getSingleResult();
   }
 
@@ -64,3 +65,4 @@ public class ProductRepository {
         .getResultList();
   }
 }
+```
